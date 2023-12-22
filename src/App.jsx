d33b3c8c5591cd528fb0b92df1288cccb232b17c
mauricio-rodriguez <mauricio_gtrodri@hotmail.com>
2023-12-22 19:45:57 -0300

    cambio de estrucura

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,61 @@
-import { useState, useEffect } from 'react'
-import axios from "axios"
-import "bootstrap/dist/css/bootstrap.min.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
-import './App.css'
+import './App.css';
 
 function App() {
-
-  const [usuario, setUsuario] = useState([])
-  const [tablausuario, settablaUsuario] = useState([])
-  const [busqueda, setBusqueda] = useState("")
+  const [usuarios, setUsuarios] = useState([]);
+  const [tablaUsuarios, setTablaUsuarios] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
 
   const peticionGet = async () => {
-    await axios.get("https://jsonplaceholder.typicode.com/users")
-      .then(response => {
-        setUsuario(response.data)
-        settablaUsuario(response.data)
-      }).catch(error => {
-        console.log(error)
-      })
-  }
+    try {
+      const response = await axios.get("https://raw.githubusercontent.com/mauriciorodriguezgit/busqueda-de-md8/main/src/assets/planos.json");
+      setUsuarios(response.data.productos);
+      setTablaUsuarios(response.data.productos);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
 
-  // captando lo escrito
   const handleChange = e => {
-    setBusqueda(e.target.value)
-    filtrar(e.target.value)
-  }
-  // filtrado de busqueda
+    setBusqueda(e.target.value);
+    filtrar(e.target.value);
+  };
+
   const filtrar = (terminoDeBusqueda) => {
-    let resultadosBusqueda = tablausuario.filter((elemento) => {
-      if (elemento.name.toString().toLowerCase().includes(terminoDeBusqueda.toLowerCase())
-        || elemento.company.name.toString().toLowerCase().includes(terminoDeBusqueda.toLowerCase())) {
-        return elemento
-      }
-    })
-    setUsuario(resultadosBusqueda)
-  }
+    let resultadosBusqueda = tablaUsuarios.filter((elemento) => {
+      return (
+        elemento.codigo.toString().toLowerCase().includes(terminoDeBusqueda.toLowerCase()) ||
+        elemento.descripcion.toString().toLowerCase().includes(terminoDeBusqueda.toLowerCase())
+      );
+    });
+    setUsuarios(resultadosBusqueda);
+  };
+
+  const handleBuscarClick = () => {
+    filtrar(busqueda);
+  };
 
   useEffect(() => {
-
-    peticionGet()
-
-  }, [])
+    peticionGet();
+  }, []);
 
   return (
-    <div className=" App">
-
-      {/* buscador */}
+    <div className="App">
+      {/* Buscador */}
       <div className='containerInput'>
-        <input className='form-control inputBuscar'
+        <input
+          className='form-control inputBuscar'
           value={busqueda}
-          placeholder='Busqueda por nombre o Empresa'
-          onChange={handleChange} />
-
-
-        <button className='btn btn-succes' >
+          placeholder='Búsqueda por código o descripción'
+          onChange={handleChange}
+        />
+        <button className='btn btn-success' onClick={handleBuscarClick}>
           <FontAwesomeIcon icon={faSearch} />
         </button>
       </div>
@@ -65,34 +64,27 @@ function App() {
         <table className='table'>
           <thead>
             <tr className='table-primary'>
-              <th >Codigo</th>
-              <th>Descripcion</th>
-              <th>Vastago</th>              
+              <th>Código</th>
+              <th>Descripción</th>
+              <th>Vástago 1</th>
+              <th>Vástago 2</th>
             </tr>
           </thead>
           <tbody>
-            {usuario && usuario.map((usuario) => (
+            {usuarios.map((usuario) => (
               <tr key={usuario.id}>
-                <td className='table-info'>{usuario.id}</td>
-                <td>{usuario.name}</td>
-                <td>{usuario.phone}</td>
-                <td>{usuario.username}</td>
-                <td>{usuario.email}</td>
-                <td>{usuario.website}</td>
-                <td>{usuario.address.city}</td>
-                <td>{usuario.company.name}</td>
+                <td className='table-info'>{usuario.codigo}</td>
+                <td>{usuario.descripcion}</td>
+                <td>{Array.isArray(usuario.vastago) ? usuario.vastago[0] : usuario.vastago}</td>
+                <td>{Array.isArray(usuario.vastago) && usuario.vastago.length > 1 ? usuario.vastago[1] : null}</td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
 
+export default App;
 
-
-
-
-
-export default App
